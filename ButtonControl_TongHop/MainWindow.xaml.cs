7903a2e6c87d680edf99b6819f29436feb4ee4cf
zigu080804 <kiethtse180309@fpt.edu.vn>
2025-06-13 23:06:15 +0700

    Nộp bài WPF 1

---
+++ ButtonControl_TongHop/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ButtonControl_TongHop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnThoat_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void BtnTong_Click(object sender, RoutedEventArgs e)
+        {
+            int a = int.Parse(txtA.Text);
+            int b = int.Parse(txtB.Text);
+            int tong = a + b;
+            tbKetQua.Text = tong + "";
+        }
+    }
+}
--- TryBathandLoop/obj/Debug/net9.0/TryBathandLoop.AssemblyInfo.cs
+++ TryBathandLoop/obj/Debug/net9.0/TryBathandLoop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TryBathandLoop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c29ecfd92ef71cf3ccdb59cad654c013d8a04d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TryBathandLoop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TryBathandLoop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
